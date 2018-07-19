b265cbf1a1c5d24aee55e9e97cbed20b7aa73121
Mike Hernandez <mkhern@gmail.com>
2018-07-19 09:25:09 -0700

    Moves App into components folder.

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Header from './components/header';
-import './styles.css';
+import Header from './header';
+import '../styles.css';
 
 class App extends Component {
   constructor(props) {

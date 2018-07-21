cb4286bb3251686582507b27b5d05bcb372af24d
Mike Hernandez <mkhern@gmail.com>
2018-07-21 11:25:45 -0700

    Adds blank area under sparkline, set up for gradient.

--- src/client/components/Sparkline.jsx
+++ src/client/components/Sparkline.jsx
@@ -3,11 +3,12 @@ import Dimensions from 'react-dimensions';
 import * as d3 from 'd3';
 import { scaleTime, scaleLinear } from '../../../node_modules/d3-scale';
 import { extent } from '../../../node_modules/d3-array';
+import { area } from '../../../node_modules/d3-shape';
 
 
 class Sparkline extends Component {
   
-  _drawGraph() {
+  _drawLine() {
     const {
       history, containerHeight, containerWidth, priceDidIncrease
     } = this.props;
@@ -25,7 +26,7 @@ class Sparkline extends Component {
       .domain(extent(data, d => d.price))
       .range([containerHeight, 0]);
     
-    let chart = d3.line()
+    let lineChart = d3.line()
       .x(d => xScale(d.time))
       .y(d => yScale(d.price));
     
@@ -34,9 +35,38 @@ class Sparkline extends Component {
         {"sparkline-path " + (priceDidIncrease ? 
           "sparkline-path--green" : "sparkline-path--red")
         }
-        d={chart(history)}
-        fill="none"
+        d={lineChart(history)}
       />
+    )
+  }
+
+  _drawArea() {
+    const {
+      history, containerHeight, containerWidth, priceDidIncrease
+    } = this.props;
+
+    const data = history.map(el => {
+      el.time = new Date(el.time);
+      return el;
+    })
+    
+    let xScale = scaleTime()
+      .domain(extent(data, d => d.time))
+      .range([0, containerWidth]);
+
+    let yScale = scaleLinear()
+      .domain(extent(data, d => d.price))
+      .range([containerHeight, 0]);
+
+    let areaChart = area()
+      .x(d => xScale(d.time))
+      .y0(containerHeight)
+      .y1(d => yScale(d.price));
+
+    return(
+      <path className="sparkline-area"
+        d={areaChart(history)}
+      ></path>
     )
   }
   
@@ -46,7 +76,8 @@ class Sparkline extends Component {
         width={this.props.containerWidth}
         height={this.props.containerHeight}
       >
-        {this._drawGraph()}
+        {this._drawLine()}
+        {this._drawArea()}
       </svg>
     )
   }

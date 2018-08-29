d35451420d752eef33d637f23e003c601e8e88be
Mike Hernandez <mkhern@gmail.com>
2018-08-28 19:14:15 -0700

    Removes unnecessary helper and fixes bug related to 24 hr time.

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,4 @@
 const moment = require('moment');
-const tz = require('moment-timezone');
 
 const adjustMissingPrices = (history) => {
   let lastValidValue = 0;
@@ -15,34 +14,10 @@ const adjustMissingPrices = (history) => {
   });
 }
 
-const getTime = () => {
-  const time = moment().tz('America/New_York');
-
-  return {
-    hour: time.format('H'),
-    minute: time.format('m'),
-    day: time.day()
-  }
-}
-
-const isDuringTradingHours = () => {
-  const time = getTime();
-
-  const isTradingDay = () => (time.day < 6);
-  const isTradingHours = () => {
-    return (
-      (time.hour >= 9 && time.minute >= 30) ||
-      (time.hour <= 16)
-    )
-  }
-
-  return (isTradingDay() && isTradingHours());
-}
-
 const formatTimeAndDate = (time, date) => {
   const dateAndTime = `${date} ${time}`;
   return moment(dateAndTime, 'YYYYMMDD hh:mm')
-    .format('YYYY-MM-DD hh:mm');
+    .format('YYYY-MM-DD HH:mm');
 }
 
 

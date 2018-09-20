41722dbe3d4e3a8cbdc5f670c5ffc1120aceeca6
Mike Hernandez <mkhern@gmail.com>
2018-09-20 15:04:39 -0700

    Renders chart axes for intraday data.

--- src/client/components/StockDetail/PriceTimeseries.jsx
+++ src/client/components/StockDetail/PriceTimeseries.jsx
@@ -3,8 +3,29 @@ import Dimensions from 'react-dimensions';
 import * as d3 from 'd3';
 import { scaleTime, scaleLinear } from 'd3-scale';
 import { extent } from 'd3-array';
+import { axisBottom, axisRight } from 'd3-axis';
+
+const chartMargins = { top: 20, right: 60, bottom: 50 };
 
 class PriceTimeseries extends Component {
+  constructor(props) {
+    super(props);
+    this.xAxis = React.createRef();
+    this.yAxis = React.createRef();
+  }
+  
+  _createAxes() {
+    const xNode = this.xAxis.current;
+    const yNode = this.yAxis.current;
+
+    const scales = this._createScales();
+
+    const xAxis = axisBottom().scale(scales.x).ticks(d3.timeHour);
+    const yAxis = axisRight().scale(scales.y).ticks(5);
+    
+    d3.select(xNode).call(xAxis);
+    d3.select(yNode).call(yAxis);
+  }
 
   _createScales() {
     const { data, containerHeight, containerWidth } = this.props;
@@ -16,11 +37,13 @@ class PriceTimeseries extends Component {
 
     const x = scaleTime()
       .domain(extent(timeSeries, d => d.date))
-      .range([0, containerWidth]);
+      .range([0, containerWidth - chartMargins.right])
+      .nice();
 
     const y = scaleLinear()
       .domain(extent(timeSeries, d => d.close))
-      .range([containerHeight, 0]);
+      .range([containerHeight - chartMargins.bottom, chartMargins.top])
+      .nice();
 
     return { x, y };
   }
@@ -40,7 +63,7 @@ class PriceTimeseries extends Component {
   }
 
   _drawArea() {
-    const { data, containerHeight, containerWidth } = this.props;
+    const { data, containerHeight } = this.props;
 
     const scale = this._createScales();
 
@@ -54,27 +77,40 @@ class PriceTimeseries extends Component {
     )
   }
 
+
+  componentDidMount() {
+    this._createAxes();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.data !== prevProps.data) {
+      this._createAxes();
+    }
+  }
+
+
   render() {
     const { containerWidth, containerHeight } = this.props
 
-    return(
+    return (
       <div className="stockdetail-price">
-        <svg className="pricechart"
-          width={containerWidth}
-          height={containerHeight}
-        >
-        <linearGradient id="pricechart-area--gradient"
-          gradientUnits="userSpaceOnUse"
-          x1="0" x2="0" y1="0" y2="250"
-        >
-          <stop offset="0%" stopColor="rgba(95,103,255,0.3)"></stop>
-          <stop offset="100%" stopColor="black"></stop>
-        </linearGradient>
-        {this._drawLine()}
-        {this._drawArea()}
+        <svg className="pricechart" width={containerWidth} height={containerHeight}>
+          <linearGradient id="pricechart-area--gradient"
+            gradientUnits="userSpaceOnUse" x1="0" x2="0" y1="0" y2="250">
+            <stop offset="0%" stopColor="rgba(95,103,255,0.3)" />
+            <stop offset="100%" stopColor="black" />
+          </linearGradient>
+          {this._drawLine()}
+          {this._drawArea()}
+          <g ref={this.xAxis}
+            className="pricechart-axis"
+            transform={`translate(0, ${containerHeight - chartMargins.top})`} />
+          <g ref={this.yAxis}
+            className="pricechart-axis"
+            transform={`translate(${containerWidth - chartMargins.right}, 0)`} />
         </svg>
       </div>
-    )
+    );
   }
 }
 
